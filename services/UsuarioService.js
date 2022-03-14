1217a8fcdbaa97138401be8e8ec44082207755a2
PauloScotti <paulo.scotti@hotmail.com>
2022-03-14 20:23:18 -0300

    pequeno ajuste services

--- services/UsuarioService.js
+++ services/UsuarioService.js
@@ -7,9 +7,12 @@ export default class UsuarioService extends HttpService {
         localStorage.setItem("nome", data.nome);
         localStorage.setItem("email", data.email);
         localStorage.setItem("token", data.token);
+
+        const usuario = await this.get('/usuario');
+        localStorage.setItem('id', usuario.data._id);
         
-        if (data.avatar) {
-            localStorage.setItem("avatar", data.avatar);
+        if (usuario.data.avatar) {
+            localStorage.setItem("avatar", usuario.data.avatar);
         }
     }
 
